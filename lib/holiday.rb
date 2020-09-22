78b88900a8c24f6d5993419d7841f3fba198847d
DavidRMorphew <morphewd@umich.edu>
2020-09-22 15:40:45 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # holiday_hash[:winter].collect {|holiday, supplies| supplies}.flatten
-  holiday_hash[:winter].values.flatten  
+  #holiday_hash[:winter].values.flatten
+  holiday_hash[:winter].collect do |holiday, supplies|
+    supplies
+  end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
